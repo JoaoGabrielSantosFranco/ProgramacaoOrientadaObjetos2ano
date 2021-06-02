42ebe6344af09778b448e3d9c5012e7a0023388c
JoaoGabrielSantosFranco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2021-06-02 16:18:47 -0300

    Add files via upload

---
+++ Tempo.java
@@ -0,0 +1,96 @@
+package pacote;
+
+class Tempo {
+
+    private int horas;
+    private int minutos;
+    private int segundos;
+    private int milisegundos;
+
+    public Tempo() {
+        this(0, 0, 0,0);
+    }
+
+
+
+    public Tempo(int horas, int minutos, int segundos, int milisegundos) {
+        setHoras(horas);
+        setMinutos(minutos);
+        setSegundos(segundos);
+        setMilisegundos(milisegundos);
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public int getMilisegundos() {
+        return milisegundos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+
+    public void setMilisegundos(int milisegundos) {
+        this.milisegundos = milisegundos;
+    }
+
+    public void imprimir() {
+        System.out.println(this);
+    }
+
+    public Tempo subtrair(Tempo tempo) {
+
+            return milisegundosTempo((this.emMilisegundos() - tempo.emMilisegundos())+3600000*24);
+
+    }
+
+    public Tempo somar(Tempo tempo) {
+            return milisegundosTempo(this.emMilisegundos() + tempo.emMilisegundos());
+    }
+
+    public Tempo inicialreal (Tempo tempo){
+        return milisegundosTempo((this.emMilisegundos()));
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d:%02d", getHoras(), getMinutos(), getSegundos(),getMilisegundos());
+    }
+
+    private int emMilisegundos() {
+        return (getHoras() * 3600000) + (getMinutos() * 60000) + getSegundos() * 1000 + getMilisegundos();
+    }
+
+    private static Tempo milisegundosTempo ( int totalMilisegundos) {
+
+            int milisegundos = totalMilisegundos;
+            int horas = milisegundos / 3600000;
+            milisegundos -= (horas * 3600000);
+            int minutos = milisegundos / 60000;
+            milisegundos -= (minutos * 60000);
+            int segundos = milisegundos / 1000;
+            milisegundos -= (segundos * 1000);
+            if (totalMilisegundos > 86400000) {
+            horas -= 24;
+            }
+            return new Tempo(horas, minutos, segundos, milisegundos);
+    }
+}
---
+++ principal.java
@@ -0,0 +1,25 @@
+package pacote;
+
+public class principal {
+    public static void main(String[] args) {
+        Tempo adicionar = new Tempo();
+        Tempo Inicial = new Tempo(12, 00, 0 ,0);
+        Tempo Variavel = new Tempo(6, 20, 0,0);
+        Tempo Sub = Inicial.subtrair(Variavel);
+        Tempo soma = Inicial.somar(Variavel);
+        Tempo real = Inicial.inicialreal(Inicial) ;
+
+
+        System.out.print("horario inicial :  " );
+        real.imprimir();
+        System.out.print("horario informado :  ");
+        Variavel.imprimir();
+        System.out.print("horario inicial menos horario informado :  ");
+        Sub.imprimir();
+        System.out.print("horario inicial mais horario informado :  " );
+        soma.imprimir();
+
+
+
+    }
+}
