68a2f591f9eedce3566b20977091a0c72244f0d2
JoaoGabrielSantosFranco <80074087+JoaoGabrielSantosFranco@users.noreply.github.com>
2021-06-24 20:30:41 -0300

    Add files via upload

---
+++ MauseGamer.java
@@ -0,0 +1,16 @@
+package pack;
+
+public class MauseGamer extends Mouse{
+
+    public void Botoes() {
+        System.out.println("Mouse tem 6 botoes clicaveis ");
+    }
+    @Override
+    public void Leitor() {
+        System.out.println("pode chegar até 15000 DPI");
+    }
+    public void sist() {
+        System.out.println("Sistema de controle de LEDS");
+    }
+
+}
